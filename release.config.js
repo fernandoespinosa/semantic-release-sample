c32397d4d197ea6e3e1b900af561b4b2c0af240e
Fernando Espinosa <fernando@polymath.network>
2019-04-17 18:54:46 -0400

    fix: trying with `release.config.js`

---
+++ release.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    branches: [
+        '+([1-9])?(.{+([1-9]),x}).x',
+        'master',
+        'next',
+        'next-major',
+        {
+            name: 'beta',
+            prerelease: true
+        },
+        {
+            name: 'alpha',
+            prerelease: true
+        }
+    ],
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/npm',
+        '@semantic-release/git',
+        '@semantic-release/github'
+    ]
+}
